d3a77987c195fa9a90d4ce43b15e352741793ea0
Geetartha Bordoloi <geetarthabordoloi936@gmail.com>
2025-05-19 15:44:06 +0530

    added income page with addincome form and barchart

--- frontend/finance-tracker/src/components/layouts/Navbar.jsx
+++ frontend/finance-tracker/src/components/layouts/Navbar.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import { HiOutlineMenu, HiOutlineX } from 'react-icons/hi';
 import SideMenu from './SideMenu';
+import { useNavigate } from 'react-router-dom';
 
 const Navbar = ({activeMenu}) => {
     const [openSideMenu, setOpenSideMenu] = useState(false);
+    const navigate = useNavigate();
   return (
     <div className='flex gap-5 bg-white border border-b border-gray-200/50 backdrop-blur-[2px] py-4 px-7 sticky top-0 z-30'>
         <button 
@@ -19,7 +21,7 @@ const Navbar = ({activeMenu}) => {
         )}
         </button>
 
-        <h2 className='text-lg font-medium text-black'>Expense Tracker</h2>
+        <h2 className='text-lg font-medium text-black' onClick={() => navigate('/dashboard')}>Expense Tracker</h2>
 
         {openSideMenu && (
             <div className='fixed top-[61px] -ml-4 bg-white'>
