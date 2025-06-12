80b353a50c4290368480d3f3fb045efb4b295ffd
Geetartha Bordoloi <geetarthabordoloi936@gmail.com>
2025-06-12 21:20:22 +0530

    fixed the expense link bug

--- frontend/finance-tracker/src/components/Modal.jsx
+++ frontend/finance-tracker/src/components/Modal.jsx
@@ -7,16 +7,16 @@ const Modal = ({children, isOpen, onClose, title}) => {
   return <div className='fixed top-0 right-0 left-0 z-0 flex items-center justify-center w-full h-[calc(100%-1rem)] max-h-full overflow-y-auto overflow-x-hidden bg-black/20 bg-opacity-50'>
         <div className='relative p-4 w-full max-w-2xl max-h-full'>
             {/* model content */}
-            <div className='relative bg-white rounded-lg shadow-sm dark:bg-gray-700'>
+            <div className='relative bg-white rounded-lg shadow-sm '>
                 {/* model header */}
-                <div className='flex items-center justify-between p-4 md:p-5 border-b border-gray-200 rounded-t dark:border-gray-200'>
-                    <h3 className='text-lg font-medium text-gray-900 dark:text-white'>
+                <div className='flex items-center justify-between p-4 md:p-5 border-b border-gray-200 rounded-t'>
+                    <h3 className='text-lg font-medium text-gray-900'>
                         {title}
                     </h3>
 
                     <button 
                         type='button'
-                        className='text-gray-400 p-1.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm inline-flex items-center justify-center dark:hover:bg-gray-600 dark:hover:text-white cursor-pointer'
+                        className='text-gray-400 p-1.5 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm inline-flex items-center justify-center cursor-pointer'
                         onClick={onClose}
                     >
                         <svg
